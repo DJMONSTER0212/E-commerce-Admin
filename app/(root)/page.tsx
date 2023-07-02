2d15e57d1f305cbd674961f48bb44acb9e3820c7
DJ <97788700+DJMONSTER0212@users.noreply.github.com>
2023-07-02 21:29:36 +0530

    enviornment setup done and shadcn ui setup also done

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+// import Image from 'next/image'
+
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+    return (
+        <div className="p-4" ><Button variant={"default"} size={"default"}>Hello</Button></div>
+    )
+}
