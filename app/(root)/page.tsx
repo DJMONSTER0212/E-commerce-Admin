70b211dc6c5ddfb53bb6a85ca60f2f445ea8a56c
DJ <97788700+DJMONSTER0212@users.noreply.github.com>
2023-07-06 16:21:36 +0530

    added modal and modal provider to resolve hydration error

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,25 @@
-// import Image from 'next/image'
+"use client";
 
-import { Button } from "@/components/ui/button";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
 
+// zustand for global state management
+// import { Button } from "@/components/ui/button";
+// import { Modal } from "@/components/ui/modal";
+// import { UserButton } from "@clerk/nextjs";
 const SetupPage = () => {
+    const onOpen = useStoreModal((state)=>state.onOpen);
+    const isOpen = useStoreModal((state)=>state.isOpen);
+
+    useEffect(()=>{
+        if(!isOpen){
+            onOpen();
+        }
+    },[isOpen,onOpen])
     return (
         <div className="p-4" >
-        This is a protected route!
+            {/* <UserButton afterSignOutUrl="/"/> */}
+            RootPage
         </div>
     )
 }
