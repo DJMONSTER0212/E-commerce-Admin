f38f9c2193b78cdf505c00ddfdf8ee11be7f7ba7
DJ <97788700+DJMONSTER0212@users.noreply.github.com>
2023-07-18 22:17:59 +0530

    added darkmode and lightmode functionality

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,6 +4,7 @@ import {MainNav} from './MainNav'
 import StoreSwitcher from './store.switcher'
 import { redirect } from 'next/navigation'
 import prismadb from '@/lib/prismadb'
+import { ModeToggle } from './theme-toggle'
 
 const Navbar = async () => {
     const {userId} = auth();
@@ -18,9 +19,11 @@ const Navbar = async () => {
     return (
         <div className='border-b'>
             <div className='flex h-16 items-center px-4'>
+
                 <StoreSwitcher items={stores}/>
                 <MainNav className='mx-6'/>
                 <div className='ml-auto flex items-center space-x-4'>
+                    <ModeToggle/>
                     <UserButton afterSignOutUrl='/'/>
 
                 </div>
